36a107e035c9fce54a924b863f6d0e4d9378d6bc
Joseguera <joseguera@uclaextension.edu>
2023-11-12 16:08:27 -0800

    Finished Progression functionality

--- src/app/components/PageTracker.tsx
+++ src/app/components/PageTracker.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 
 type PageProps = {
   pages: {
@@ -10,29 +10,51 @@ type PageProps = {
   pageCount: number;
 }
 
-const PageTracker: FC<PageProps> = ({ pages }) => {
+const PageTracker: FC<PageProps> = ({ pages, pageCount }) => {
+
+  const [barProgress, setBarProgress] = useState('w-0');
+
+  function getBarProgress() {
+    if (pageCount === 1) {
+      setBarProgress(prevProgress => prevProgress = "w-0");
+    } else if (pageCount === 2) {
+      setBarProgress(prevProgress => prevProgress = "w-1/4");
+    } else if (pageCount === 3) {
+      setBarProgress(prevProgress => prevProgress = "w-2/4");
+    } else if (pageCount === 4) { 
+      setBarProgress(prevProgress => prevProgress = "w-3/4");
+    } else {
+      setBarProgress(prevProgress => prevProgress = "w-full");
+    }
+  }
+
+  useEffect(() => {
+    getBarProgress();
+  }, [pageCount, getBarProgress])
+
+  console.log(barProgress)
+
   return (
     <div className="w-[438px] py-14 flex flex-col align-center">
       <div className="flex flex-row justify-between items-center z-10">
-        <div className="w-5 h-5 rounded-full bg-[#FFA724] flex justify-center items-center">
-          <div className="w-3 h-3 rounded-full bg-[#FFFFFF99]"></div>
+        <div className={`w-5 h-5 rounded-full bg-[${pageCount >= 1 ? "#FFA724" : "#6D6D6D"}] flex justify-center items-center`}>
+          <div className={`w-3 h-3 rounded-full bg-[${pageCount === 1 && "#FFFFFF99"}]`}></div>
         </div>
-        <div className="w-5 h-5 rounded-full bg-[#FFA724] flex justify-center items-center">
-          <div className="w-3 h-3 rounded-full bg-[#FFFFFF99]"></div>
+        <div className={`w-5 h-5 rounded-full bg-[${pageCount >= 2 ? "#FFA724" : "#6D6D6D"}] flex justify-center items-center`}>
+          <div className={`w-3 h-3 rounded-full bg-[${pageCount === 2 && "#FFFFFF99"}]`}></div>
         </div>
-        <div className="w-5 h-5 rounded-full bg-[#FFA724] flex justify-center items-center">
-          <div className="w-3 h-3 rounded-full bg-[#FFFFFF99]"></div>
+        <div className={`w-5 h-5 rounded-full bg-[${pageCount >= 3 ? "#FFA724" : "#6D6D6D"}] flex justify-center items-center`}>
+          <div className={`w-3 h-3 rounded-full bg-[${pageCount === 3 && "#FFFFFF99"}]`}></div>
         </div>
-        <div className="w-5 h-5 rounded-full bg-[#FFA724] flex justify-center items-center">
-          <div className="w-3 h-3 rounded-full bg-[#FFFFFF99]"></div>
+        <div className={`w-5 h-5 rounded-full bg-[${pageCount >= 4 ? "#FFA724" : "#6D6D6D"}] flex justify-center items-center`}>
+          <div className={`w-3 h-3 rounded-full bg-[${pageCount === 4 && "#FFFFFF99"}]`}></div>
         </div>
-        <div className="w-5 h-5 rounded-full bg-[#6D6D6D] flex justify-center items-center">
-          <div className="w-3 h-3 rounded-full bg-[#FFFFFF99]"></div>
+        <div className={`w-5 h-5 rounded-full bg-[${pageCount >= 5 ? "#FFA724" : "#6D6D6D"}] flex justify-center items-center`}>
+          <div className={`w-3 h-3 rounded-full bg-[${pageCount === 5 && "#FFFFFF99"}]`}></div>
         </div>
       </div>
       <div className="h-1 w-[97%] pl-[11px] relative top-[-12px] bg-[#6D6D6D]">
-        <div className="h-1 w-3/4 bg-[#FFA724]"></div>
-        {/* <div className="h-1 w-full bg-[#FFA724]"></div> */}
+        <div className={`h-1 ${barProgress} bg-[#FFA724]`}></div>
       </div>
     </div>
   );
